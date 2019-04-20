dfcd03b250a15732f9d63c135f7a40ecf40839e3
Lizzy Presland <lizzy.presland@gmail.com>
2019-04-19 18:14:17 -0700

    BE ONE WITH THE CROB

--- src/newcaw/newcaw.py
+++ src/newcaw/newcaw.py
@@ -5,16 +5,20 @@ Post will be saved as a raw text file because REEEEEEEEEEEEEEEE
 """
 
 from flask import Blueprint, send_from_directory
-
+from flask import render_template
+from flask import redirect
+from flask import url_for
+import time
 newcaw = Blueprint('newcaw', __name__, url_prefix='/caw')
 
-@newcaw.route('/', methods=['GET'])
+@newcaw.route('/', methods=['GET', 'POST'])
 def root():
-    return send_from_directory('static', filename='newcaw.html')
+    return render_template('newcaw.html')
 
+@newcaw.route('/caw/', methods=['GET', 'POST'])
 def make_new_crob_post():
     """
-    Display a page with an input field and a submit button.
     TODO: validate auth status for logged in user? Add username?
+    TODO: string replacement
     """
-    return "the method got called, hurrah"
+    return redirect(url_for('landing'))
