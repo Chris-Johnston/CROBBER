023cdcc54dc4a6f444d363d1fc12491e8616cee9
Lizzy Presland <lizzy.presland@gmail.com>
2019-04-19 19:46:29 -0700

    CROB LIVES! :warning:

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-from flask import Flask, send_from_directory, session, url_for, request, flash, redirect
+from flask import Flask, send_from_directory, session, url_for, request, flash, redirect, render_template
 # from flask import register_blueprint
 from authlib.flask.client import OAuth
-from newcaw.newcaw import newcaw as newcaw
+# from newcaw.newcaw import newcaw as newcaw
 import os
 
 app = Flask(__name__)
@@ -10,12 +10,19 @@ app = Flask(__name__)
 
 # https://discordapp.com/api/oauth2/authorize?client_id=568868246909091850&redirect_uri=localhost%3A5000&response_type=code&scope=identify
 
-app.register_blueprint(newcaw)
+# app.register_blueprint(newcaw)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/index.html')
 def landing():
-    return send_from_directory('static', filename='landing.html')
+    return render_template('landing.html')
+    # return send_from_directory('te', filename='landing.html')
+
+# post a new "caw"
+@app.route('/caw/', methods=['GET', 'POST'])
+def cawcaw():
+    # TODO: actual functional logic to add the post to the database
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
